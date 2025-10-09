922908422fe7c8bd5347f1dcc2411d0f11936db1
C4m174 <Osaosita7@gmail.com>
2025-10-08 20:31:17 -0500

    docs: correciones nueva version

--- app/src/main/java/com/stoq/StockWise/MainActivity.kt
+++ app/src/main/java/com/stoq/StockWise/MainActivity.kt
@@ -3,11 +3,9 @@ package com.stoq.StockWise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import androidx.activity.enableEdgeToEdge
-import com.stoq.StockWise.Iam.presentation.navigation.authNavigation
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.stoq.StockWise.Iam.presentation.navigation.NavigationAuth
 import com.stoq.StockWise.Iam.presentation.viewmodels.AuthViewModel
 import com.stoq.StockWise.shared.data.local.JwtStorage
 import com.stoq.StockWise.ui.theme.StockWiseTheme
@@ -18,18 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         JwtStorage.initialize(this)
-
         setContent {
             StockWiseTheme {
-                val navController = rememberNavController()
                 val authViewModel: AuthViewModel = viewModel()
-
-                NavHost(
-                    navController = navController,
-                    startDestination = "login"
-                ) {
-                    authNavigation(navController, authViewModel)
-                }
+                NavigationAuth(authViewModel = authViewModel)
             }
         }
     }
