d753fa00e028df60200fa2ef437a847ea9a9ea4e
Krillsom <kevinjchic@gmail.com>
2025-10-08 22:28:25 -0500

    feat: add get products view functional

--- app/src/main/java/com/stoq/StockWise/MainActivity.kt
+++ app/src/main/java/com/stoq/StockWise/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         JwtStorage.initialize(this)
+        
         setContent {
             StockWiseTheme {
                 val authViewModel: AuthViewModel = viewModel()
